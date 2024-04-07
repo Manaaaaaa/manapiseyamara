a85e97afe2763c7421fb4e7047b2d681c9230306
AmaraThorng <156154353+amarathorng@users.noreply.github.com>
2024-04-07 21:51:28 +0700

    Add files via upload

---
+++ exercise-3.py
@@ -0,0 +1,13 @@
+def remove_all_after(numbers, n):
+    ...
+    if not numbers:
+        return []
+    try:
+        index = numbers.index(n)
+        return numbers[:index + 1]
+    except ValueError:
+        return numbers
+print(remove_all_after([1, 2, 3, 4, 5], 3))  
+print(remove_all_after([1, 1, 2, 2, 3, 3], 2)) 
+
+    
