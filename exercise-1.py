149c84ca363747e9185f97a1112807a9b1d61d55
Kimsourmana <156396910+Manaaaaaa@users.noreply.github.com>
2024-04-07 22:40:11 +0800

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,9 @@
 def replace_last(numbers):
-    pass
+    if  len(numbers) <= 1:
+      return numbers
+    else:
+      return [numbers[-1]] + numbers[:-1]
+
+numbers_input = input("Enter numbers separated by commas: ").strip()
+numbers_list = [int(num) for num in numbers_input.split(',')]
+print(replace_last(numbers_list))
